06f5cbd4f61e8dff9f2f36c5af1832e269c74ce4
Krishna47ak <anandakrishna720@gmail.com>
2023-06-11 23:35:47 +0530

    Improvements in profileDetail component

--- Frontend/agri-market/src/components/profile/ProfileDetail.js
+++ Frontend/agri-market/src/components/profile/ProfileDetail.js
@@ -3,10 +3,13 @@ import React from 'react'
 const ProfileDetail = ({ title, info, isAddress }) => {
     return (
         <div>
-            <div className='flex bg-white p-3 px-5 rounded-xl justify-between' >
-                <span className='font-bold mr-3' >{title} :</span>
+            <div className='flex bg-white p-3 px-5 rounded-xl space-x-5' >
+                <div className='flex w-44 justify-between pr-3' >
+                    <span className='font-bold ' >{title} </span>
+                    <p>:</p>
+                </div>
                 {isAddress ? (
-                    <textarea disabled={true} value={info} className='font-semibold w-96 ' />
+                    <textarea disabled={true} value={info} className='font-semibold w-80 ' />
                 ) : (
                     <p className='font-semibold' >{info} </p>
                 )}
